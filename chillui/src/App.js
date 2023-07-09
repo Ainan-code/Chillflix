045ff5349a3565e2325bec1bfa2770fb78a3211f
Ainan-code <ainanhassan123@gmail.com>
2023-07-09 18:25:26 +0300

    initiated header component

---
+++ chillui/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Header from './includes/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ chillui/src/includes/Header.js
@@ -0,0 +1,19 @@
+ import React from "react";
+
+ function Header() {
+    return (
+        <div className="wrapper">
+        <div><p>Chillflix</p></div>
+        <div>
+        <ul>
+          <li><a href="#">Homepage</a></li>
+          <li><a href="#">Categories</a></li>
+          <li><a href="#">About</a></li>
+        </ul>
+        </div>
+      </div> )
+    
+    }
+
+    
+ export default Header;
